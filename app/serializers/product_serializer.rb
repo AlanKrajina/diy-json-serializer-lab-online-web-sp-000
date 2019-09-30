3600c0d4fa52c6ad8a86845ad1f5d1ec778bccf0
Alan Krajina <alankrajina88@gmail.com>
2019-09-30 16:11:56 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,8 +6,11 @@ class ProductSerializer
     serialized_product = '{'
 
     serialized_product += '"id": ' + product.id.to_s + ', '
-    serialized_product += '"title": "' + post.title + '", '
-    serialized_product += '"description": "' + post.description + '", '
+    serialized_product += '"title": "' + product.title + '", '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"price": "' + product.price + '", '
+
+    serialized_product += '"description": "' + product.description + '", '
 
     # the author association can also be represented in JSON
     serialized_product += '"author": {'
